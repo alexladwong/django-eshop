a33250fdd96dc4b2e7c99712243c670cb1c3dd89
AMBASSADOR ALEX LADWONG <81810294+alexladwong@users.noreply.github.com>
2026-02-02 08:19:37 +0300

    Updated
    Removed sensitive information from settings.

--- MY_COM/settings.py
+++ MY_COM/settings.py
@@ -22,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rb0b42+kh2erkyz(h37^n@cx3_8jao#nwc9talkjfpg-3xdulf"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
